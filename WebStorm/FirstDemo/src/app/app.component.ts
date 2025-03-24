91a5d3a4fd9ac3f49d45dcbdbb97a285d124b93b
AlexRovere <75780598+AlexRovere@users.noreply.github.com>
2025-03-24 13:37:06 +0100

    init angular demo

---
+++ WebStorm/FirstDemo/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+import {HeaderComponent} from './shared/header/header.component';
+import {FooterComponent} from './shared/footer/footer.component';
+import {HomeComponent} from './pages/home/home.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent implements OnInit{
+  title = 'FirstDemo';
+  ngOnInit(): void {
+    initFlowbite();
+  }
+}
