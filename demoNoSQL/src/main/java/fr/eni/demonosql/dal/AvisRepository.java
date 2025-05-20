7b8d60ff014a50d3f6d498c0a2fb4edf2dd69bd7
AlexRovere <wormz91@hotmail.fr>
2025-05-20 09:49:51 +0200

    machine learning

--- demoNoSQL/src/main/java/fr/eni/demonosql/dal/AvisRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.eni.demonosql.dal;
-
-import fr.eni.demonosql.bo.Avis;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AvisRepository extends MongoRepository<Avis, String> {
-}
